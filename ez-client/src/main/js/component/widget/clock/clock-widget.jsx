3d20456e7ccddd49b41a744d29d8e85fba1f0e5e
ylacaute <yannick.lacaute@gmail.com>
2020-02-01 00:30:21 +0100

    Refactoring Clock widget and add stories

--- ez-client/src/main/js/component/widget/clock/clock-widget.jsx
+++ ez-client/src/main/js/component/widget/clock/clock-widget.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {bool} from 'prop-types';
 import Widget from "component/widget/base/widget";
 import WidgetContent from "component/widget/base/widget-content";
 import DateService from "service/date/date-service";
+import ScalableText from 'component/scalable/text';
 import cn from "classnames";
 
 import "./clock-widget.scss";
 
 export default class ClockWidget extends React.PureComponent {
 
-  static propTypes = Object.assign({
-    UTCOffset: PropTypes.number
-  }, Widget.propTypes);
+  static propTypes = {
+    ...Widget.propTypes,
+    displayTime: bool
+  };
 
   static defaultProps = {
-    UTCOffset: 2
+    displayTime: true
+  };
+
+  interval = null;
+
+  state = {
+    className: "",
+    localDate: "--",
+    time: "--"
   };
 
   constructor(props) {
@@ -22,6 +32,15 @@ export default class ClockWidget extends React.PureComponent {
     this.state = {
       ...this.getClockData()
     };
+  }
+
+  static getDerivedStateFromProps(props) {
+    return {
+      className: cn("clock", props.className)
+    }
+  }
+
+  componentDidMount() {
     this.inteval = setInterval(() => {
       this.setState({
         ...this.getClockData()
@@ -35,34 +54,32 @@ export default class ClockWidget extends React.PureComponent {
 
   getClockData() {
     let now = DateService.now();
-    let hours = now.getUTCHours() + this.props.UTCOffset;
-    let minutes = now.getUTCMinutes();
-    let seconds = now.getUTCSeconds();
-    if (hours >= 24) hours -= 24;
-    if (hours < 0) hours += 12;
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
     if (hours < 10) hours = "0" + hours;
     if (minutes < 10) minutes = "0" + minutes;
     if (seconds < 10) seconds = "0" + seconds;
     return {
-      date: now.toLocaleDateString("fr-FR"),
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds
+      localDate: now.toLocaleDateString("fr-FR").toString(),
+      time: `${hours}:${minutes}:${seconds}`
     };
   }
 
   render() {
-    const {date, hours, minutes, seconds} = this.state;
-    const classNames = cn("clock", this.props.className);
+    const {className, localDate, time} = this.state;
+    const {displayTime} = this.props;
 
     return (
       <Widget
-        className={classNames}
+        className={className}
         {...this.props}
       >
         <WidgetContent>
-          <p>{date}</p>
-          <p>{hours}:{minutes}:{seconds}</p>
+          <ScalableText text={localDate}/>
+          { displayTime &&
+            <ScalableText text={time} fontSize={10}/>
+          }
         </WidgetContent>
       </Widget>
     )
