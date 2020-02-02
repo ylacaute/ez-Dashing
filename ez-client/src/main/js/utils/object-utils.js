bcb617cabb5b2d106e48dcf5848bd99f69cb995f
ylacaute <yannick.lacaute@gmail.com>
2020-02-02 17:37:46 +0100

    Add mapValuesDeep utils function

--- ez-client/src/main/js/utils/object-utils.js
+++ ez-client/src/main/js/utils/object-utils.js
@@ -1,3 +1,5 @@
+import {map, isArray, isObject, isPlainObject, mapValues} from "lodash"
+
 export default class ObjectUtils {
 
   /**
@@ -7,6 +9,21 @@ export default class ObjectUtils {
     let text = Function.prototype.toString.call(object.constructor);
     return text.match(/function (.*)\(/)[1];
   };
+
+  /**
+   * Credits: https://github.com/Kikobeats/map-values-deep
+   * Should be replaced by a native Lodash function
+   * This function has been copied in order to bring some modifications in the future.
+   */
+  static mapValuesDeep(obj, fn, key) {
+    return isArray(obj)
+      ? map(obj, (innerObj, idx) => ObjectUtils.mapValuesDeep(innerObj, fn, idx))
+      : isPlainObject(obj)
+        ? mapValues(obj, (val, key) => ObjectUtils.mapValuesDeep(val, fn, key))
+        : isObject(obj)
+          ? obj
+          : fn(obj, key)
+  }
 
 }
 
--- ez-client/src/test/js/utils/object-utils.test.js
+++ ez-client/src/test/js/utils/object-utils.test.js
@@ -19,4 +19,55 @@ describe("ObjectUtils", () => {
     expect(ObjectUtils.typeOf(instance)).toStrictEqual("SampleClass");
   });
 
+  const square = (value, key) => value * value;
+  const keyPlusValue = (value, key) => key + value;
+
+  it("mapValuesDeep() should pass on empty object", () => {
+    const result = ObjectUtils.mapValuesDeep({}, square);
+    expect(result).toStrictEqual({});
+  });
+
+  it("mapValuesDeep() should transform simple object", () => {
+    const result = ObjectUtils.mapValuesDeep({ two: 2, three: 3 }, square);
+    expect(result).toStrictEqual({ two: 4, three: 9 });
+  });
+
+  it("mapValuesDeep() should transform simple array", () => {
+    const result = ObjectUtils.mapValuesDeep([2, 3], square);
+    expect(result).toStrictEqual([4, 9]);
+  });
+
+  it("mapValuesDeep() should transform object with nested objects/arrays", () => {
+    let input = { two: 2, obj: { three: 3, four: 4 }, arr: [5, 6] };
+    let expectedOutput = { two: 4, obj: { three: 9, four: 16 }, arr: [25, 36] };
+
+    const result = ObjectUtils.mapValuesDeep(input, square);
+    expect(result).toStrictEqual(expectedOutput);
+  });
+
+  it("mapValuesDeep() should transform array with nested objects/arrays", () => {
+    const result = ObjectUtils.mapValuesDeep([2, { three: 3, four: 4 }, [5, 6]], square);
+    expect(result).toStrictEqual([
+      4,
+      { three: 9, four: 16 },
+      [25, 36]
+    ]);
+  });
+
+  it("mapValuesDeep() should pass object key to mapper function", () => {
+    const result = ObjectUtils.mapValuesDeep({ key1: "foo", key2: "bar" }, keyPlusValue);
+    expect(result).toStrictEqual({ key1: "key1foo", key2: "key2bar" });
+  });
+
+  it("mapValuesDeep() should pass array key (index) to mapper function", () => {
+    const result = ObjectUtils.mapValuesDeep([0, 2, 4], keyPlusValue);
+    expect(result).toStrictEqual([0, 3, 6]);
+  });
+
+  it("mapValuesDeep() should do nothing on non plain objects", () => {
+    const now = new Date();
+    const result = ObjectUtils.mapValuesDeep(now);
+    expect(result).toStrictEqual(now);
+  });
+  
 });
